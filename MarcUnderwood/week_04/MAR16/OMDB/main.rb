440dc531ead897f01d87d5eba4d91e0401398c0f
mgundies <mgundies@mgundiess-MacBook-Pro.local>
2016-03-17 10:30:25 +1100

    handles no result searches

--- MarcUnderwood/week_04/MAR16/OMDB/main.rb
+++ MarcUnderwood/week_04/MAR16/OMDB/main.rb
@@ -8,45 +8,29 @@ get "/" do
 end
 
 get "/results" do
-  # @search = params[:search]
-  # str = ("http://omdbapi.com/?t=" + @search).to_s
-  # @result = HTTParty.get str
-  # @poster = @result["Poster"]
-  # erb :results
-
-  # @search = params[:search]
-  # # str = ("http://omdbapi.com/st=" + @search).to_s
-  # @result = HTTParty.get("http://omdbapi.com/?t=#{@search}")
-  # @poster = @result["Poster"]
-  # erb :results
-
   @search = params[:search]
-  # str = ("http://omdbapi.com/st=" + @search).to_s
-  @result_array = HTTParty.get("http://omdbapi.com/?s=#{@search}")["Search"]
-  @ln = @result_array.length
   @total = HTTParty.get("http://omdbapi.com/?s=#{@search}")
-  @num_titles = @total["totalResults"].to_s
-  @pages= ((@total["totalResults"].to_f/@ln).ceil).to_i
-  @r_title = []
-  @r_title << @result_array[0]
-  # @r_title.push(@result_array[2])
-
-  for @i in (1 .. @pages)
-    @result_array = HTTParty.get("http://omdbapi.com/?s=#{@search}&page=#{@i}")["Search"]
-    for @j in (0 .. @ln-1)
-      if (!(@result_array[@j] == nil))
-        @r_title << @result_array[@j]
+  p @total
+  puts @total["Error"]
+  if @total["Error"] == "Movie not found!"
+    @num_titles = "Movie not found!"
+    erb:results
+  else
+    @num_titles = @total["totalResults"].to_s
+    @result_array = HTTParty.get("http://omdbapi.com/?s=#{@search}")["Search"]
+    @ln = @result_array.length
+    @pages= ((@total["totalResults"].to_f/@ln).ceil).to_i
+    @r_title = []
+    @r_title << @result_array[0]
+    for @i in (1 .. @pages)
+      @result_array = HTTParty.get("http://omdbapi.com/?s=#{@search}&page=#{@i}")["Search"]
+      for @j in (0 .. @ln-1)
+        if (!(@result_array[@j] == nil))
+          @r_title << @result_array[@j]
+        end
       end
     end
+    @r_title.shift
+    erb :results
   end
-  @r_title.shift
-
-
-  # @x = HTTParty.get("http://omdbapi.com/?s=#{@search}&page=2")["Search"]
-  # @result_array.push(@x)
-
-  erb :results
-
-
-
 end
